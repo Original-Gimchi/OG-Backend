df3e6e1e078b4e6b112163975aba3735a1a90400
jacobhboy <jacobhboy66@gmail.com>
2022-09-23 00:01:48 +0900

    add : Post

--- src/main/java/og/bumawiki/bumawiki/BumawikiApplication.java
+++ src/main/java/og/bumawiki/bumawiki/BumawikiApplication.java
@@ -2,8 +2,10 @@ package og.bumawiki.bumawiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BumawikiApplication {
 
 	public static void main(String[] args) {
