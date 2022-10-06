d498fe77233f7602d2ce5d2d1326167c79fb7821
jacobhboy <jacobhboy66@gmail.com>
2022-10-07 08:18:32 +0900

    Add : PostService

--- src/main/java/og/bumawiki/bumawiki/domain/post/service/PostService.java
+++ src/main/java/og/bumawiki/bumawiki/domain/post/service/PostService.java
@@ -3,12 +3,20 @@ package og.bumawiki.bumawiki.domain.post.service;
 import lombok.RequiredArgsConstructor;
 import og.bumawiki.bumawiki.domain.post.domain.Post;
 import og.bumawiki.bumawiki.domain.post.domain.repository.PostRepository;
-import og.bumawiki.bumawiki.domain.post.web.request.PostCreateRequestDto;
+import og.bumawiki.bumawiki.domain.post.web.dto.request.PostCreateRequestDto;
+import og.bumawiki.bumawiki.domain.post.web.dto.response.PostResponseDto;
+import og.bumawiki.bumawiki.domain.user.repository.UserRepository;
+import og.bumawiki.bumawiki.global.config.security.SecurityUtil;
+import og.bumawiki.bumawiki.global.exception.CustomException;
+import og.bumawiki.bumawiki.global.exception.ErrorCode;
 import okhttp3.OkHttpClient;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Pageable;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ import java.awt.print.Pageable;
 public class PostService {
     private final PostRepository postRepository;
     private final OkHttpClient okHttpClient;
+    private final UserRepository userRepository;
 
     @Transactional
     public Long createPost(PostCreateRequestDto request){
@@ -24,5 +33,37 @@ public class PostService {
         return post.getId();
     }
 
+    public List<PostResponseDto> findByTitle(String title, Pageable pageable){
+        return postRepository.findByTitle(title, pageable)
+                .stream()
+                .map(PostResponseDto :: new)
+                .collect(Collectors.toList());
+    }
 
+    public List<PostResponseDto> findByViewDesc(Pageable pageable){
+        return postRepository.findByViewDesc(pageable)
+                .stream()
+                .map(PostResponseDto :: new)
+                .collect(Collectors.toList());
+    }
+
+    public List<PostResponseDto> findAll(Pageable pageable){
+        return postRepository.findAll(pageable)
+                .stream()
+                .map(PostResponseDto :: new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public PostResponseDto update(Long id, PostCreateRequestDto request){
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        userRepository.findByEmail(SecurityUtil.getLoginUserName())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_LOGIN));
+
+        post.update(request.getTitle(), request.getContent());
+
+        return new PostResponseDto(post);
+    }
 }
