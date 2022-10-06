22be4c4169b9707cd42a4a761eaacfa72e112d14
jacobhboy <jacobhboy66@gmail.com>
2022-10-07 01:50:45 +0900

    Add : PostRepository

--- src/main/java/og/bumawiki/bumawiki/domain/post/domain/repository/PostRepository.java
+++ src/main/java/og/bumawiki/bumawiki/domain/post/domain/repository/PostRepository.java
@@ -1,5 +1,6 @@
-package og.bumawiki.bumawiki.domain.post;
+package og.bumawiki.bumawiki.domain.post.domain.repository;
 
+import og.bumawiki.bumawiki.domain.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
