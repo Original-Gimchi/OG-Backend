3321172c70d4548aa9bc5e37da4456b14f2fbaf4
jacobhboy <jacobhboy66@gmail.com>
2022-10-07 01:48:00 +0900

    Update : UserAndRole

--- src/main/java/og/bumawiki/bumawiki/domain/user/domain/User.java
+++ src/main/java/og/bumawiki/bumawiki/domain/user/domain/User.java
@@ -1,12 +1,10 @@
-package og.bumawiki.bumawiki.domain.user;
+package og.bumawiki.bumawiki.domain.user.domain;
 
 import lombok.*;
+import og.bumawiki.bumawiki.domain.user.domain.type.Role;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,10 +13,15 @@ public class User {
     @Id @GeneratedValue
     private Long id;
 
+    @Column(length = 32)
     private String email; // 학교 email
 
     @Column(length = 4)
     private String name;
+
+    @Column(length = 8)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Column(length = 4)
     private String classNumber;
@@ -27,11 +30,11 @@ public class User {
     private String password;
 
     @Builder
-    public User(Long id, String email, String name, String classNumber, String password) {
-        this.id = id;
+    public User(String email, String name, String classNumber, String password,Role role) {
         this.email = email;
         this.name = name;
         this.classNumber = classNumber;
         this.password = password;
+        this.role = role;
     }
 }
